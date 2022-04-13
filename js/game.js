1b812dbe01711ff77092bc8df7cc2441b7949ae3
Malik <99354681+maliksalem1@users.noreply.github.com>
2022-04-13 13:17:22 -0400

    code

---
+++ js/game.js
@@ -0,0 +1,11 @@
+/* global phaser */
+
+const config = {
+    type: Phaser.Auto,
+    width: 1920,
+    height: 1080,
+    backgroundcolor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
