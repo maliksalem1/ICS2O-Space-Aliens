86afc6c60a9b14390fd61fbf15f2ce9dbd742d9c
Malik <99354681+maliksalem1@users.noreply.github.com>
2022-04-19 17:26:58 +0000

    code

--- js/game.js
+++ js/game.js
@@ -5,20 +5,20 @@
 // This is the config file for the phaser3 program
 
 //* Game scene */
-const config = {
+const config = { 
   type: Phaser.Auto,
   width: 1920,
   height: 1080,
   physics: {
-    default: 'arcade'
+    default: 'arcade',
     arcade: {
       debug: true
     }
   },
   // set background color
-  backgroundcolor: 0x5f6e7a
+  backgroundcolor: 0x5f6e7a,
   scale: {
-    mode: Phase.Scale.FIT,
+    mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
     autocenter: Phaser.Scale.CENTER_BOTH
   }
