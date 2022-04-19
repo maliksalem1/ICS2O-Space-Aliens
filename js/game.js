f5294c2c4b7ece1c5b9a6fbb3742d9be5084584f
Malik <99354681+maliksalem1@users.noreply.github.com>
2022-04-19 16:43:42 +0000

    code

--- js/game.js
+++ js/game.js
@@ -1,10 +1,27 @@
 /* global phaser */
 
+// Created by: maliksalem1
+// Created on April 2022
+// This is the config file for the phaser3 program
+
+//* Game scene */
 const config = {
-    type: Phaser.Auto,
-    width: 1920,
-    height: 1080,
-    backgroundcolor: 0x5f6e7a
+  type: Phaser.Auto,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade'
+    arcade: {
+      debug: true
+    }
+  }
+  // set background color
+  backgroundcolor: 0x5f6e7a
+  scale: {
+    mode: Phase.Scale.FIT,
+    // we place it in the middle of the page.
+    autocenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
