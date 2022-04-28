7dd63f374021b5ef076a07771c71ff67c5a18871
Malik <99354681+maliksalem1@users.noreply.github.com>
2022-04-28 01:51:30 +0000

    some code

--- js/game.js
+++ js/game.js
@@ -5,18 +5,21 @@
 // This is the config file for the phaser3 program
 
 // scene import statements
-import SplashScene from './js/splashScene.js'
+import SplashScene from "./splashScene.js"
 
-// our game scene
-const splashScene = new SplashScene()
+// create the new scene
+const splashscene = new SplashScene()
+const titlescene = new TitleScene()
 
-//* Game Scene */
+/**
+ * Start Phaser Game
+ */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
   physics: {
-    default: 'arcade',
+    default:'arcade',
     arcade: {
       debug: true
     }
@@ -25,16 +28,18 @@ const config = {
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
-    // we place it in the middle of the page
+    // we place it in the middle of the page.
     autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+   }
 }
 
 const game = new Phaser.Game(config)
+// console.log(game)
 
-// load scenes 
-// NOTE: remember any "key" is global and CAN NOT be reused
-game.secne.add('splashScene', splashScene)
+// load scene 
+// NOTE: remeber any "key" is global and CAN NOT be reused
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
 
 // start title
-game.scene.start('splashScene')
+game.scene.start("splashScene")
